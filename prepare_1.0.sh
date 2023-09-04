0812f639b81f3b067fb15b23ccb1493838896c72
HoneyMack <tosimiti7@gmail.com>
2023-09-04 11:20:08 +0900

    softgymの環境設定用シェルスクリプトを改良

--- prepare_1.0.sh
+++ prepare_1.0.sh
@@ -1,4 +1,9 @@
-. activate softgym
+
 export PYFLEXROOT=${PWD}/PyFlex
 export PYTHONPATH=${PYFLEXROOT}/bindings/build:$PYTHONPATH
 export LD_LIBRARY_PATH=${PYFLEXROOT}/external/SDL2-2.0.4/lib/x64:$LD_LIBRARY_PATH
+export PYTHONPATH=${PWD}:$PYTHONPATH
+
+username="robot_dev4"
+source "/home/${username}/anaconda3/etc/profile.d/conda.sh"
+conda activate softgym
