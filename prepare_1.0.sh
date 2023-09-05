51f1b7ba89e70e5ad929941e21656ae6c6b6346c
HoneyMack <tosimiti7@gmail.com>
2023-09-05 15:05:02 +0900

    マシン名依存性を下げた

--- prepare_1.0.sh
+++ prepare_1.0.sh
@@ -4,6 +4,6 @@ export PYTHONPATH=${PYFLEXROOT}/bindings/build:$PYTHONPATH
 export LD_LIBRARY_PATH=${PYFLEXROOT}/external/SDL2-2.0.4/lib/x64:$LD_LIBRARY_PATH
 export PYTHONPATH=${PWD}:$PYTHONPATH
 
-username="robot_dev4"
+username=${HOST_USER_NAME}
 source "/home/${username}/anaconda3/etc/profile.d/conda.sh"
 conda activate softgym
